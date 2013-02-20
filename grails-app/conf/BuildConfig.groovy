d8e7c5fe6a69206b3974005fca0534ae4bbedcc4
samcday <sam.c.day@gmail.com>
2013-02-20 12:29:27 +1000

    Need "legacyResolve true" in BuildConfig for publish-plugin to work correctly.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,7 +6,7 @@ grails.project.dependency.resolution = {
     inherits("global") {
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+    legacyResolve true // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
     repositories {
         grailsCentral()
     }
